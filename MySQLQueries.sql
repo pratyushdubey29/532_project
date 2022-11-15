30d2f85bb9458af4f27ef42e9cde1b99128a0a43
Anshumaan-Chauhan02 <98472023+Anshumaan-Chauhan02@users.noreply.github.com>
2022-11-15 10:46:00 -0500

    Add files via upload

--- MySQLQueries.sql
+++ MySQLQueries.sql
@@ -15,3 +15,17 @@ Alter table sys.dataset add label int;
 # If slightly delayed = 0, highly delayed =1, other =2
 UPDATE  sys.dataset
 SET     sys.dataset.label = IF((sys.dataset.CancellationCode='N' and sys.dataset.Diverted=0 and sys.dataset.ArrDelay<=15),0, IF((sys.dataset.CancellationCode='N' and sys.dataset.Diverted=0 and sys.dataset.ArrDelay>15),1,2));
+
+# Checking the distribution of the label values
+select sys.dataset.label, count(sys.dataset.label)
+from sys.dataset
+group by sys.dataset.label;
+
+# Sub splitting the value 2 (other) into 2 values - Diverted=3, Cancelled=2
+UPDATE  sys.dataset
+SET     sys.dataset.label = IF((sys.dataset.CancellationCode!='N'),2, IF((sys.dataset.Diverted=1),3,sys.dataset.label));
+
+#Checking the distribution of delayedness, diverted, and cancelled 
+select sys.dataset.label, count(sys.dataset.label)
+from sys.dataset
+group by sys.dataset.label;
