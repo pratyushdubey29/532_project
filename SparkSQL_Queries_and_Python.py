b9441264cf46c29c43d1774e015553699c588b62
Anshumaan-Chauhan02 <98472023+Anshumaan-Chauhan02@users.noreply.github.com>
2022-11-18 10:55:53 -0500

    Add files via upload

--- SparkSQL_Queries_and_Python.py
+++ SparkSQL_Queries_and_Python.py
@@ -33,3 +33,22 @@ dataset.select(*(col(c) for c in dataset.columns)).where((col("TaxiIn").isNull()
 dataset.select(*(col(c) for c in dataset.columns)).where((col("TaxiIn").isNull() | isnan(col("TaxiIn"))) & (col("Diverted")==0) & (col("CancellationCode")=='N')).count()
 
 #As we can infer above the TaxiIn are ArrTime are Null a few times. But it is the case when the flight is either cancelled or diverted. So it is fine if we not exclude the null values
+
+dataset.select(*(col(c) for c in dataset.columns)).where((col("TaxiOut").isNull() | isnan(col("TaxiOut")))).show(vertical=True)
+
+dataset.select(*(col(c) for c in dataset.columns)).where(((col("TaxiOut").isNull() | isnan(col("TaxiOut")))) & (col("CancellationCode")=='N')).show(vertical=True)
+
+#We can infer from the above queries that TaxiOut is null only in the case of a cancelled flight. So it is fine if these are converted to 0s after type casting
+
+dataset.select(*(col(c) for c in dataset.columns)).where((col("CRSElapsedTime").isNull() | isnan(col("CRSElapsedTime")))).show(vertical=True)
+
+dataset.select(*(col(c) for c in dataset.columns)).where((col("CRSElapsedTime").isNull() | isnan(col("CRSElapsedTime"))) & (col("Diverted")==0)).count()
+
+#We can infer from the above queries that CRSElapsedTime is null only in the case of a diverted flight. So it is fine if these are converted to 0s after type casting
+# Last columns have null values if the flight is cancelled instead of getting delayed, we do not drop these columns. Instead when we type cast the column to numeric data type these will be converted to zero which makes sense
+
+dataset.count()
+
+dataset.select("_c0").distinct().count()
+
+#Means that _c0 column is just for indexing the entreis, therefore we will use this column for joining the 2 datasets
